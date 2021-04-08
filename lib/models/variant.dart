8ebad27b2b30f9c719c3f994b1f66aaf9f332f2b
naokikotani <nok.naoki.tigers.415@icloud.com>
2021-04-09 04:55:39 +0900

    variant修正

--- lib/models/variant.dart
+++ lib/models/variant.dart
@@ -1,20 +1,18 @@
 class Variant {
-  const Variant({this.id, this.imageUrl, this.price, this.productType});
+  const Variant({this.id, this.price, this.imageUrl, this.productType});
 
   factory Variant.fromJson(Map<String, dynamic> parsedJson) {
     return Variant(
-      id: parsedJson['variants']['edges'][0]
-          ['node']['id'] as String,
-      imageUrl: parsedJson[''] as String ?? 'https://www.shoshinsha-design.com/wp-content/uploads/2020/05/noimage-1-580x440.png',
-      productType: parsedJson['variants']
-          ['edges'][0]['node']['title'] as String,
-      price: parsedJson['variants']['edges'][0]
-          ['node']['price'] as String,
+      id: parsedJson['variants']['edges'][0]['node']['id'] as String,
+      price: parsedJson['variants']['edges'][0]['node']['price'] as String,
+      imageUrl: parsedJson[''] as String ??
+          'https://www.shoshinsha-design.com/wp-content/uploads/2020/05/noimage-1-580x440.png',
+      productType: parsedJson['variants']['edges']['node']['title'] as String,
     );
   }
 
-  final String price;
   final String id;
+  final String price;
   final String imageUrl;
   final String productType;
 }
