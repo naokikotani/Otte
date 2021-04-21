1d5d0fc2e4ee4f536d7b3cce7a133700549b6dcb
naokikotani <nok.naoki.tigers.415@icloud.com>
2021-04-21 18:41:55 +0900

    修正

--- lib/ui/cart/viewModel.dart
+++ lib/ui/cart/viewModel.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:otte/models/product.dart';
-import 'package:otte/repositories/productRepository.dart';
 
 final cartPageViewModelProvider =
     AutoDisposeChangeNotifierProvider((ref) => CartPageViewModelProvider());
 
 class CartPageViewModelProvider extends ChangeNotifier {
   List<Product> products = [];
-  List<Product> _products = [];
 
   List<Product> get cartProducts => products;
 }
